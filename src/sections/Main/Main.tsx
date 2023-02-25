f649a4a063ce0feb327be805f832e3c9848ed595
Matt Proctor <proctormatt0@gmail.com>
2023-02-25 12:18:24 -0800

    Refactored components into separate files

--- src/sections/Main/Main.tsx
+++ src/sections/Main/Main.tsx
@@ -1,36 +1,83 @@
-import { RefObject, useEffect, useState } from "react";
-import { motion, useScroll, useTransform } from "framer-motion"
-import { Background, ProfilePicture, SocialMediaCluster, SubTitle, Title } from "./components";
-import "./main.css"
+import { RefObject, useRef } from "react";
+import { motion, useScroll, useTransform } from "framer-motion";
+import {
+  Background,
+  ProfilePicture,
+  SocialMediaCluster,
+  SubTitle,
+  Title,
+  WaveTransition,
+} from "./components";
+import "./main.css";
 
 type MainSectionProps = {
-  innerRef: RefObject<HTMLDivElement>
-}
+  innerRef: RefObject<HTMLDivElement>;
+};
 
 export const Main = (props: MainSectionProps) => {
   const { innerRef } = props;
-  const { scrollYProgress } = useScroll({
+  const contentRef = useRef(null);
+  const { scrollYProgress: scrollProgressFromTop } = useScroll({
+    target: contentRef,
+    offset: ["end end", "end start"],
+  });
+  const { scrollY } = useScroll({
     target: innerRef,
-    offset: ["end end", "end start"]
-  })
-  const scrollProgress = useTransform(scrollYProgress, [0, 1], ["0%", "50%"])
+    offset: ["center center", "end start"],
+  });
+  const waveOneSpeed = useTransform(
+    scrollProgressFromTop,
+    [0, 1],
+    ["20%", "-60%"]
+  );
+  const waveTwoSpeed = useTransform(
+    scrollProgressFromTop,
+    [0, 1],
+    ["15%", "-45%"]
+  );
+  const waveThreeSpeed = useTransform(
+    scrollProgressFromTop,
+    [0, 1],
+    ["13%", "-25%"]
+  );
+  const waveFourSpeed = useTransform(
+    scrollProgressFromTop,
+    [0, 1],
+    ["8%", "-15%"]
+  );
+  const waveFiveSpeed = useTransform(
+    scrollProgressFromTop,
+    [0, 1],
+    ["5%", "-4%"]
+  );
 
   return (
     <section
       ref={innerRef}
-      className="relative flex flex-col items-center justify-center w-full h-screen bg-cover p-28 #bg-slate-900/70 overflow-clip"
+      className="relative flex flex-col items-center justify-center w-full bg-cover p-28 overflow-clip"
     >
-      <Background scrollModifier={scrollProgress} />
-      <div className="grid grid-cols-2 w-full">
-        <div className="flex flex-col justify-center">
+      <Background scrollModifier={scrollY} />
+      <motion.div
+        ref={contentRef}
+        className="grid grid-cols-2 w-full h-screen pb-40"
+        style={{ y: scrollY }}
+      >
+        <motion.div className="flex flex-col justify-center">
           <Title />
           <SubTitle />
-          <SocialMediaCluster />
-        </div>
-        <div className="flex justify-center">
+          {/* <SocialMediaCluster /> */}
+        </motion.div>
+        <motion.div className="flex items-center justify-center">
           <ProfilePicture />
-        </div>
-      </div>
+        </motion.div>
+      </motion.div>
+      <WaveTransition
+        waveOneSpeed={waveOneSpeed}
+        waveTwoSpeed={waveTwoSpeed}
+        waveThreeSpeed={waveThreeSpeed}
+        waveFourSpeed={waveFourSpeed}
+        waveFiveSpeed={waveFiveSpeed}
+      />
     </section>
-  )
-}
+  );
+};

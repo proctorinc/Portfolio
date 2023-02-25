ffc74cc73c75bd445e805c9636fbe58222b0fd16
Matt Proctor <proctormatt0@gmail.com>
2023-02-25 12:10:37 -0800

    reformatted with prettier

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,9 @@
 const Footer = () => {
   return (
-    <footer className="flex bottom-0 text-center text-slate-400 gap-5 justify-center p-3 font-extralight">
+    <footer className="bottom-0 text-center text-slate-400 gap-5 justify-center p-3 font-extralight bg-transparent">
       <span>&#169; 2023 Proctor Inc</span>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;

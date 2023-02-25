fc4ea6c8c09cce5ae07ecbb24a95a62c832276fa
Matt Proctor <proctormatt0@gmail.com>
2023-02-25 12:18:03 -0800

    Removed pattern and formatted with prettier

--- src/sections/Main/components/ProfilePicture.tsx
+++ src/sections/Main/components/ProfilePicture.tsx
@@ -1,8 +1,11 @@
-import { motion } from "framer-motion"
+import { PatternLines } from "@visx/pattern";
+import { Bar } from "@visx/shape";
+import { motion } from "framer-motion";
 
 export const ProfilePicture = () => {
   return (
-    <motion.div className="border-2 border-slate-600 rounded-xl w-72 h-72 bg-slate-400/20"
+    <motion.div
+      className="border-2 border-slate-600 rounded-xl w-80 h-80 bg-slate-500/20"
       initial={{
         scale: 0,
       }}
@@ -13,10 +16,21 @@ export const ProfilePicture = () => {
           duration: 1.5,
           type: "spring",
           stiffness: 40,
-        }
+        },
       }}
     >
+      <svg className="w-full h-full">
+        <PatternLines
+          id="smallerlines"
+          stroke="rgb(255, 255, 255, 0.01)"
+          strokeWidth={2}
+          width={50}
+          height={50}
+          orientation={["vertical", "horizontal"]}
+        />
+        <Bar fill={`url(#smallerlines)`} width="100%" height="100%" />
+      </svg>
       {/* Add profile picture here */}
     </motion.div>
-  )
-}
+  );
+};

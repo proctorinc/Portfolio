8178211398a4fe9e40ce7c6aabd90349cb64c434
Matt Proctor <proctormatt0@gmail.com>
2023-02-25 12:09:50 -0800

    Added tailwind custom box shadow

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        "inner-2xl": "inset 0 25px 50px -12px rgb(0 0 0 / 0.25)",
+      },
+    },
   },
   plugins: [],
 };

b167ad1ef7aa17df7788ad0c08522e96db32580e
Matt Proctor <proctormatt0@gmail.com>
2023-02-25 21:43:24 -0800

    Made footer absolute to overlap content

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 export const Footer = () => {
   return (
-    <footer className="bottom-0 justify-center gap-5 bg-transparent p-3 text-center font-extralight text-slate-400">
+    <footer className="absolute fixed bottom-0 w-full justify-center gap-5 bg-transparent p-3 text-center font-extralight text-slate-400">
       <span>&#169; 2023 Proctor Inc</span>
     </footer>
   );

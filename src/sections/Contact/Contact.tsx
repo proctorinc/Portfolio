c5828cbd63f16a6ba87f0f150eec39dc9b6205e1
Matt Proctor <proctormatt0@gmail.com>
2023-02-25 21:44:06 -0800

    Added animations to contact section

--- src/sections/Contact/Contact.tsx
+++ src/sections/Contact/Contact.tsx
@@ -1,8 +1,7 @@
 import { RefObject } from "react";
-import { useScroll } from "framer-motion";
+import { useScroll, motion } from "framer-motion";
 
 import { SocialMediaCluster } from "./components";
-import { Card, GridBackground } from "../../components";
 import { SmallWaveTransition } from "../../components/Transitions"
 
 type ContactSectionProps = {
@@ -18,24 +17,34 @@ export const Contact = (props: ContactSectionProps) => {
 
   return (
     <section
+      id="contact"
       ref={innerRef}
       className="gradient-light-to-dark relative flex w-full flex-col items-center justify-center pt-64"
     >
       <SmallWaveTransition scrollModifier={scrollY} />
       <div className="flex h-screen items-center justify-center">
-        <Card className="relative">
-          <GridBackground />
-          <div className="flex flex-col gap-2 p-10 text-center">
-            <h1 className="text-5xl font-extrabold">Matt Proctor</h1>
-            <h2 className="text-xl font-extralight text-slate-300">
-              matthewalanproctor@gmail.com
-            </h2>
-            <h2 className="text-xl font-extralight text-slate-300">
-              425.295.4143
-            </h2>
-            <SocialMediaCluster />
-          </div>
-        </Card>
+        <motion.div
+          className="flex flex-col gap-2 text-center"
+          initial={{
+            opacity: 0,
+          }}
+          whileInView={{
+            opacity: 1,
+            transition: {
+              duration: 1,
+              ease: "easeIn"
+            }
+          }}  
+        >
+          <h1 className="text-7xl font-extrabold py-5">Matt Proctor</h1>
+          <h2 className="text-xl font-extralight text-slate-300">
+            matthewalanproctor@gmail.com
+          </h2>
+          <h2 className="text-xl font-extralight text-slate-300">
+            425.295.4143
+          </h2>
+          <SocialMediaCluster />
+        </motion.div>
       </div>
     </section>
   );

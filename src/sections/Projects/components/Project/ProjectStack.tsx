f89b76a363a4fe0c68f35b1429b15f3366b5afe4
Matt Proctor <proctormatt0@gmail.com>
2023-06-02 16:58:08 -0700

    Added Tag

--- src/sections/Projects/components/Project/ProjectStack.tsx
+++ src/sections/Projects/components/Project/ProjectStack.tsx
@@ -1,31 +1,7 @@
-import { motion } from "framer-motion";
+import { Tag } from "../../../../components/Tag";
 
 type StackClusterProps = {
   stack: string[];
-};
-
-type StackItemProps = {
-  name: string;
-};
-
-const StackItem = (props: StackItemProps) => {
-  const { name } = props;
-  return (
-    <motion.div
-      className="select-none rounded-md bg-slate-400/20 py-1 px-2 text-slate-300 hover:bg-slate-400/50"
-      initial={{
-        scale: 1,
-      }}
-      whileHover={{
-        scale: 1.2,
-        transition: {
-          duration: 0.2,
-        },
-      }}
-    >
-      <span>{name}</span>
-    </motion.div>
-  );
 };
 
 export const ProjectStack = (props: StackClusterProps) => {
@@ -34,7 +10,9 @@ export const ProjectStack = (props: StackClusterProps) => {
   return (
     <div className="flex flex-wrap gap-1 text-xs">
       {stack.map((name, i) => (
-        <StackItem key={i} name={name} />
+        <Tag key={i}>
+          <span>{name}</span>
+        </Tag>
       ))}
     </div>
   );

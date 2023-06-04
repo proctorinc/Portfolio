b72818b10677d73b9be5160e36a7bb6cfde621a8
Matt Proctor <proctormatt0@gmail.com>
2023-06-04 13:59:57 -0700

    Removed comments. Fixed gap issue

--- src/sections/Experience/Experience.tsx
+++ src/sections/Experience/Experience.tsx
@@ -128,7 +128,7 @@ export const Experience = (props: MainSectionProps) => {
         </div>
       </div>
       <motion.div
-        className="absolute bottom-0 z-50 w-full bg-[#121f39] sm:-mt-32"
+        className="absolute bottom-0 z-50 mt-2 w-full bg-[#121f39] sm:-mt-32"
         style={{ height: boxHeight }}
       />
       <motion.svg
@@ -147,22 +147,6 @@ export const Experience = (props: MainSectionProps) => {
           className="mt-20"
         ></motion.path>
       </motion.svg>
-      {/* <div className="absolute -bottom-2 w-full">
-        <svg
-          className="flex-no-shrink bottom-0 w-full fill-current"
-          viewBox="0 0 900 600"
-          xmlns="http://www.w3.org/2000/svg"
-          xmlnsXlink="http://www.w3.org/1999/xlink"
-          version="1.1"
-        >
-          <motion.path
-            d="M0 455L16.7 448C33.3 441 66.7 427 100 415.8C133.3 404.7 166.7 396.3 200 406.2C233.3 416 266.7 444 300 458.3C333.3 472.7 366.7 473.3 400 462.2C433.3 451 466.7 428 500 427.3C533.3 426.7 566.7 448.3 600 464.8C633.3 481.3 666.7 492.7 700 500.8C733.3 509 766.7 514 800 494.3C833.3 474.7 866.7 430.3 883.3 408.2L900 386L900 601L883.3 601C866.7 601 833.3 601 800 601C766.7 601 733.3 601 700 601C666.7 601 633.3 601 600 601C566.7 601 533.3 601 500 601C466.7 601 433.3 601 400 601C366.7 601 333.3 601 300 601C266.7 601 233.3 601 200 601C166.7 601 133.3 601 100 601C66.7 601 33.3 601 16.7 601L0 601Z"
-            fill="#121f39"
-            strokeLinecap="round"
-            strokeLinejoin="miter"
-          ></motion.path>
-        </svg>
-      </div> */}
     </section>
   );
 };

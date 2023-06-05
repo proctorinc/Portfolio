2849bf3cb821f8e757582c27907ee3aff0154520
Matt Proctor <proctormatt0@gmail.com>
2023-06-04 17:18:14 -0700

    Solved Main section reappearing issue

--- src/sections/Main/Main.tsx
+++ src/sections/Main/Main.tsx
@@ -1,5 +1,5 @@
 import { RefObject, useRef } from "react";
-import { motion } from "framer-motion";
+import { motion, useScroll, useTransform } from "framer-motion";
 import { Background, ProfilePicture, SubTitle, Title } from "./components";
 
 type MainSectionProps = {
@@ -8,12 +8,23 @@ type MainSectionProps = {
 
 export const Main = (props: MainSectionProps) => {
   const { innerRef } = props;
+  const contentRef = useRef(null);
+
+  const { scrollYProgress } = useScroll({
+    target: contentRef,
+    offset: ["start start", "end start"],
+  });
+
+  const scrollProgress = useTransform(scrollYProgress, [1, 0], ["0%", "100%"]);
 
   return (
     <section id="home" ref={innerRef} className="-mt-20 sm:-mt-[72px]">
-      <div className="fixed -z-10 flex h-screen w-full flex-col items-center justify-center overflow-clip bg-cover px-5 pt-40 sm:px-20">
+      <motion.div
+        className="fixed -z-10 flex h-screen w-full flex-col items-center justify-center overflow-clip bg-cover px-5 pt-40 sm:px-20"
+        style={{ opacity: scrollProgress }}
+      >
         <Background />
-        <motion.div className="grid h-screen w-full grid-cols-1 pb-40 md:grid-cols-2">
+        <div className="grid h-screen w-full grid-cols-1 pb-40 md:grid-cols-2">
           <div className="sm:items-left flex h-fit w-full flex-col items-center justify-center gap-2 sm:h-full">
             <Title />
             <SubTitle />
@@ -21,9 +32,9 @@ export const Main = (props: MainSectionProps) => {
           <div className="order-first flex items-center justify-center sm:order-last">
             <ProfilePicture />
           </div>
-        </motion.div>
-      </div>
-      <div className="h-screen w-full"></div>
+        </div>
+      </motion.div>
+      <div ref={contentRef} className="h-screen w-full"></div>
     </section>
   );
 };

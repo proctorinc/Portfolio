9e102565de44e41991b51a87284e5db1a5aa1713
Matt Proctor <proctormatt0@gmail.com>
2023-07-07 10:41:29 -0700

    Fixed other projects section

--- src/sections/Projects/Projects.tsx
+++ src/sections/Projects/Projects.tsx
@@ -5,6 +5,7 @@ import PROJECTS from "../../data/projects.json";
 import { Button } from "../../components";
 import { ArrowSquareOut, Code } from "phosphor-react";
 import { ProjectStack } from "./components/Project/ProjectStack";
+import { Bar } from "@visx/shape";
 
 type ProjectsSectionProps = {
   innerRef: RefObject<HTMLDivElement>;
@@ -25,7 +26,7 @@ export const Projects = (props: ProjectsSectionProps) => {
         <Project key={projectData.title} data={projectData} index={i} />
       ))}
       <div className="flex flex-col gap-5 text-center">
-        <h2 className="pt-28 text-5xl font-extrabold">
+        <h2 className="pt-28 text-3xl font-extrabold sm:text-5xl">
           Checkout my other projects
         </h2>
         <div className="flex flex-col items-center justify-center gap-5 sm:flex-row">
@@ -42,23 +43,23 @@ export const Projects = (props: ProjectsSectionProps) => {
           </Button>
         </div>
       </div>
-      <div className="grid w-full max-w-2xl grid-cols-1 gap-5 px-3 pb-20 text-center sm:grid-cols-2 sm:px-10 lg:max-w-[1000px] lg:grid-cols-3">
+      <div className="grid w-full max-w-3xl grid-cols-1 gap-5 px-3 pb-20 text-center sm:grid-cols-2 sm:px-10 lg:max-w-5xl lg:grid-cols-3">
         {PROJECTS.slice(3).map((project) => (
           <div
             key={project.title}
-            className="flex w-full flex-col justify-center gap-3"
+            className="relative flex w-full flex-col justify-center gap-3"
           >
-            <div
-              style={{
-                backgroundImage: `/static/images/${project.image_urls[0]}`,
-              }}
-              className="group relative flex aspect-square w-full flex-col rounded-xl bg-white/10 bg-cover"
-            >
+            <div className="absolute h-full w-full rounded-xl border-2 border-slate-600 bg-slate-500/20">
+              <svg className="h-full w-full">
+                <Bar fill={`url(#smallerlines)`} width="100%" height="100%" />
+              </svg>
+            </div>
+            <div className="group relative flex aspect-square w-full flex-col rounded-xl bg-contain">
               <div className="absolute z-10 hidden h-full w-full flex-col items-center justify-center gap-5 rounded-xl bg-gray-900/50 p-3 backdrop-blur-md group-hover:flex">
-                <p className="">
-                  {project.subtitle}
-                  {project.image_urls[0]}
-                </p>
+                <div className="flex flex-col gap-3 pt-5">
+                  <h3 className="text-xl font-bold">{project.title}</h3>
+                  <p>{project.subtitle}</p>
+                </div>
                 <ProjectStack
                   className="justify-center"
                   stack={project.stack}
@@ -73,7 +74,7 @@ export const Projects = (props: ProjectsSectionProps) => {
                       }
                     >
                       <ArrowSquareOut weight="fill" size={15} />
-                      <span>Visit App</span>
+                      <span>Visit</span>
                     </Button>
                   )}
                   {project?.git_url && (
@@ -81,14 +82,24 @@ export const Projects = (props: ProjectsSectionProps) => {
                       onClick={() => (window.location.href = project.git_url)}
                     >
                       <Code weight="fill" size={15} />
-                      <span>View code</span>
+                      <span>Code</span>
                     </Button>
                   )}
                 </div>
               </div>
-              <div className="flex flex-col justify-end p-2 pt-5">
-                <h3 className="text-xl font-bold">{project.title}</h3>
-              </div>
+              {project.image_urls.length > 0 && (
+                <img
+                  src={`/static/images/${project.image_urls[0]}`}
+                  className="aspect-square rounded-xl bg-cover"
+                />
+              )}
+              {project.image_urls.length === 0 && (
+                <div className="flex h-full w-full items-center justify-center">
+                  <h3 className="text-xl font-extrabold">
+                    Image Coming Soon...
+                  </h3>
+                </div>
+              )}
             </div>
           </div>
         ))}
